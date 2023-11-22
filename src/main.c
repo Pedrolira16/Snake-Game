1ae225b8a1aa65d5dc3ec7c0b2ae4d542680b3e3
Euri <62456615+TheEuri@users.noreply.github.com>
2023-11-21 22:43:04 -0300

    Add choice of difficulty

--- src/main.c
+++ src/main.c
@@ -8,6 +8,9 @@
 #include <time.h>
 
 bool ateFood = false;
+bool startGame = false;
+int snakeSpeed = 250000;
+int page = 1;
 
 typedef struct Segment
 {
@@ -37,94 +40,150 @@ bool hasItCollided(Snake *snake);
 
 int main()
 {
-  srand(time(NULL));
   keyboardInit();
-  screenInit(1);
-  timerInit(100);
+  screenInit(0);
 
-  Snake snake;
-  Food food;
+  printf("Escolha uma dificuldade: \n");
+  printf("1 - Fácil (1.0x)\n");
+  printf("2 - Médio (1.5x)\n");
+  printf("3 - Difícil (1.7x)\n");
+  printf("4 - Insano (2.0x)\n");
+  printf("5 - Sair\n");
+  printf("Pressione a tecla correspondente.\n");
 
-  snake.head = (Segment *)malloc(sizeof(Segment));
-  snake.head->x = 10;
-  snake.head->y = 10;
-  snake.head->next = NULL;
-  snake.head->prev = NULL;
-  snake.dx = 1;
-  snake.dy = 0;
+  while (1)
+  {
 
-  generateFoodInRandomLocal(&food);
-
-  while (1) {
-    
-
-    if (keyhit()) {
+    if (keyhit())
+    {
       char key = readch();
-      switch (key) {
-        case 'w':
-          snake.dx = 0;
-          snake.dy = -1;
-          break;
-        case 'a':
-          snake.dx = -1;
-          snake.dy = 0;
-          break;
-        case 's':
-          snake.dx = 0;
-          snake.dy = 1;
-          break;
-        case 'd':
-          snake.dx = 1;
-          snake.dy = 0;
-          break;
+      switch (key)
+      {
+      case '1':
+        snakeSpeed = 250000;
+        startGame = true;
+        break;
+      case '2':
+        snakeSpeed = 200000;
+        startGame = true;
+        break;
+      case '3':
+        snakeSpeed = 170000;
+        startGame = true;
+        break;
+      case '4':
+        snakeSpeed = 140000;
+        startGame = true;
+        break;
+      case '5':
+        keyboardDestroy();
+        return 0;
       }
     }
 
-    moveSnake(&snake, snake.dx, snake.dy, &food);
-    displayGame(&snake, &food);
-    usleep(170000);
+    if (startGame)
+    {
 
-    if (hasItCollided(&snake)) {
-      break;
+      srand(time(NULL));
+      screenInit(1);
+      timerInit(100);
+
+      Snake snake;
+      Food food;
+
+      snake.head = (Segment *)malloc(sizeof(Segment));
+      snake.head->x = 10;
+      snake.head->y = 10;
+      snake.head->next = NULL;
+      snake.head->prev = NULL;
+      snake.dx = 1;
+      snake.dy = 0;
+
+      generateFoodInRandomLocal(&food);
+
+      while (1)
+      {
+
+        if (keyhit())
+        {
+          char key = readch();
+
+          switch (key)
+          {
+          case 'w':
+            snake.dx = 0;
+            snake.dy = -1;
+            break;
+          case 'a':
+            snake.dx = -1;
+            snake.dy = 0;
+            break;
+          case 's':
+            snake.dx = 0;
+            snake.dy = 1;
+            break;
+          case 'd':
+            snake.dx = 1;
+            snake.dy = 0;
+            break;
+          }
+        }
+
+        moveSnake(&snake, snake.dx, snake.dy, &food);
+        displayGame(&snake, &food);
+        usleep(snakeSpeed);
+
+        if (hasItCollided(&snake))
+        {
+          break;
+        }
+      }
+
+      screenDestroy();
+      keyboardDestroy();
+
+      return 0;
     }
   }
-
-  screenDestroy();
-  keyboardDestroy();
-
-  return 0;
 }
 
-void moveSnake(Snake *snake, int dx, int dy, Food *food) {
-    Segment *newHead = (Segment *)malloc(sizeof(Segment));
-    newHead->x = snake->head->x + dx;
-    newHead->y = snake->head->y + dy;
-    newHead->prev = NULL;
-    newHead->next = snake->head;
+void moveSnake(Snake *snake, int dx, int dy, Food *food)
+{
+  Segment *newHead = (Segment *)malloc(sizeof(Segment));
+  newHead->x = snake->head->x + dx;
+  newHead->y = snake->head->y + dy;
+  newHead->prev = NULL;
+  newHead->next = snake->head;
 
-    if (snake->head != NULL) {
-        snake->head->prev = newHead;
+  if (snake->head != NULL)
+  {
+    snake->head->prev = newHead;
+  }
+
+  snake->head = newHead;
+
+  if (newHead->x == food->x && newHead->y == food->y)
+  {
+    ateFood = true;
+    generateFoodInRandomLocal(food);
+  }
+  else
+  {
+    Segment *current = snake->head;
+    Segment *prevSegment = NULL;
+
+    while (current->next != NULL)
+    {
+      prevSegment = current;
+      current = current->next;
     }
 
-    snake->head = newHead;
-
-    if (newHead->x == food->x && newHead->y == food->y) {
-        ateFood = true;
-        generateFoodInRandomLocal(food);
-    } else {
-        Segment *current = snake->head;
-        Segment *prevSegment = NULL;
-
-        while (current->next != NULL) {
-            prevSegment = current;
-            current = current->next;
-        }
-
-        if (prevSegment != NULL) {
-            prevSegment->next = NULL;
-        }
-        free(current);
+    if (prevSegment != NULL)
+    {
+      prevSegment->next = NULL;
     }
+    free(current);
+  }
 }
 
 void generateFoodInRandomLocal(Food *food)
@@ -137,7 +196,8 @@ void displayGame(Snake *snake, Food *food)
 {
   screenInit(1);
   Segment *segment = snake->head;
-  while (segment != NULL) {
+  while (segment != NULL)
+  {
     screenGotoxy(segment->x, segment->y);
     printf("O");
     segment = segment->next;
@@ -151,13 +211,16 @@ void displayGame(Snake *snake, Food *food)
 
 bool hasItCollided(Snake *snake)
 {
-  if (snake->head->x <= 1 || snake->head->x > MAXX - 2 || snake->head->y <= 1 || snake->head->y > MAXY - 1) {
+  if (snake->head->x <= 1 || snake->head->x > MAXX - 2 || snake->head->y <= 1 || snake->head->y > MAXY - 1)
+  {
     return true;
   }
 
   Segment *segment = snake->head->next;
-  while (segment != NULL) {
-    if (snake->head->x == segment->x && snake->head->y == segment->y) {
+  while (segment != NULL)
+  {
+    if (snake->head->x == segment->x && snake->head->y == segment->y)
+    {
       return true;
     }
     segment = segment->next;
